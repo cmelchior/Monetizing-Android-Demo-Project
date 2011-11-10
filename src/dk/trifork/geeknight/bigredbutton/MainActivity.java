0be97619aebfef16bc8aafb36cf5250fbd3dead4
Christian Melchior <christian@ilios.dk>
2011-11-10 14:08:48 +0100

    "The Really Big Red Button That Does Nothing" added.

--- src/dk/trifork/geeknight/bigredbutton/MainActivity.java
+++ src/dk/trifork/geeknight/bigredbutton/MainActivity.java
@@ -2,12 +2,40 @@ package dk.trifork.geeknight.bigredbutton;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
-    /** Called when the activity is first created. */
+public class MainActivity extends Activity implements android.view.View.OnClickListener {
+    
+	private TextView buttonText;
+	private ImageButton button;
+	private String[] textList;
+	private int currentButtonNumber;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        // Set view references
+        buttonText = (TextView) findViewById(R.id.button_text);
+        button = (ImageButton) findViewById(R.id.red_button);
+        
+        // Setup button texts
+        textList = getResources().getStringArray(R.array.button_texts); 
+        currentButtonNumber = -1;
+        
+        // Setup event handlers
+        button.setOnClickListener(this);
     }
+
+    /**
+     * Handle button clicks. Loop button texts inifinitly.
+     */
+	@Override
+	public void onClick(View v) {
+		currentButtonNumber = ++currentButtonNumber % textList.length;
+		buttonText.setText(textList[currentButtonNumber]);
+	}
 }
